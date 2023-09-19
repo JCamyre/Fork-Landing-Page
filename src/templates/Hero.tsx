e17e3bb6383d16802c46b8ba52c8547dc1fc6e39
JCamyre <jwcamry03@gmail.com>
2023-09-18 20:55:30 -0700

    Start working on Fork launch site.

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -8,29 +8,17 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="bg-gray-100">
+  <Background color="bg-primary-200">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
-        </li>
-        <li>
-          <Link href="/">Sign in</Link>
-        </li>
+        <p></p>
       </NavbarTwoColumns>
     </Section>
 
     <Section yPadding="pt-20 pb-32">
       <HeroOneButton
-        title={
-          <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
-          </>
-        }
-        description="The easiest way to build a React landing page in seconds."
+        title={<span className="text-white">Fork</span>}
+        description="Your ultimate companion for effortlessly embracing healthy eating"
         button={
           <Link href="https://creativedesignsguru.com/category/nextjs/">
             <Button xl>Download Your Free Theme</Button>
