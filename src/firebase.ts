d53652b4ec2909f6c0ab62419332dbb139abd4ac
JCamyre <jwcamry03@gmail.com>
2024-02-28 19:55:11 -0800

    Add correct social media icons and links in footer.

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+// import 'firebase/firestore';
+
+// import firebase from 'firebase/app';
+
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(yourFirebaseConfig);
+// }
+
+// const db = firebase.firestore();
