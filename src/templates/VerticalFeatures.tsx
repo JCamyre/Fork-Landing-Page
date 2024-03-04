a693a90480d353efa0296a3b1d2dfa6fff0de144
JCamyre <jwcamry03@gmail.com>
2024-03-03 20:25:33 -0800

    Add figma embeded.

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -11,6 +11,20 @@ const VerticalFeatures = () => (
       title="This app is meant for everyone"
       description="No matter what your diet plan, health goals, or dietary restrictions are, Fork has you covered. We have made sure to include all major plans so that you can find the next dish for you."
       video={
+        <iframe
+          style={{ border: '1px solid rgba(0, 0, 0, 0.1);' }}
+          width="500"
+          height="1025"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F0054IIPKH2yd9qMfuNsdqU%2FFork-App-(Copy)%3Fnode-id%3D3277-4056%26mode%3Ddesign%26t%3DVNdFGqjGGevN54Jk-1"
+          // allowfullscreen
+        ></iframe>
+      }
+      videoAlt="Fork Demo"
+    />
+    <VerticalFeatureRow
+      title="This app is meant for everyone"
+      description="No matter what your diet plan, health goals, or dietary restrictions are, Fork has you covered. We have made sure to include all major plans so that you can find the next dish for you."
+      video={
         <video
           loop={true}
           autoPlay={true}
