ab77d4e8fa1e78a132036251274db04c77e5252c
JCamyre <jwcamry03@gmail.com>
2024-03-04 11:02:52 -0800

    Should fix mobile video.

--- src/templates/Video.tsx
+++ src/templates/Video.tsx
@@ -11,32 +11,11 @@ function VideoPlayer({ src, type, playbackSpeed }: VideoPlayerProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    const video = videoRef.current;
     // Check if the video element is loaded
-    if (video) {
+    if (videoRef.current) {
       // Set the playback rate to 0.5 to play the video at half speed
-      video.playbackRate = playbackSpeed;
+      videoRef.current.playbackRate = playbackSpeed;
     }
-
-    // Function to stop fullscreen
-    // const preventFullScreen = (event: Event) => {
-    //   event.preventDefault();
-    //   if (event.stopPropagation) event.stopPropagation();
-    // };
-
-    // Add event listeners to prevent fullscreen
-    // video?.addEventListener('fullscreenchange', preventFullScreen);
-    // video?.addEventListener('webkitfullscreenchange', preventFullScreen);
-    // video?.addEventListener('mozfullscreenchange', preventFullScreen);
-    // video?.addEventListener('MSFullscreenChange', preventFullScreen);
-
-    // return () => {
-    //   // Clean up event listeners
-    //   video?.removeEventListener('fullscreenchange', preventFullScreen);
-    //   video?.removeEventListener('webkitfullscreenchange', preventFullScreen);
-    //   video?.removeEventListener('mozfullscreenchange', preventFullScreen);
-    //   video?.removeEventListener('MSFullscreenChange', preventFullScreen);
-    // };
   }, []); // Empty dependency array means this effect runs once on mount
 
   return (
