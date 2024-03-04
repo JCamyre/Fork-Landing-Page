fbf74ceae69b5bfcce87f9de42b56df865cf136c
JCamyre <jwcamry03@gmail.com>
2024-03-03 18:01:53 -0800

    Fix

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,13 +2,22 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const app = initializeApp({
-  apiKey: 'process.env.apiKey',
-  authDomain: 'process.env.authDomain',
-  projectId: 'process.env.projectId',
-  storageBucket: 'process.env.storageBucket',
-  messagingSenderId: 'process.env.messagingSenderId',
-  appId: 'process.env.appId',
-  measurementId: 'process.env.measurementId',
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 });
+// console.log(
+//   process.env.apiKey,
+//   process.env.authDomain,
+//   process.env.projectId,
+//   process.env.storageBucket,
+//   process.env.messagingSenderId,
+//   process.env.appId,
+//   process.env.measurementId,
+// );
 const db = getFirestore(app);
 export default db;
